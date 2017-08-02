04d405d54e49696278ce6a5188d8bab550c7fa46
Arnout Boks <arnoutboks@gmail.com>
2017-08-02 15:04:21 +0200

    Added sniff that disallows uniqid() without $more_entropy = true
    When $more_entropy is false (which is the default), uniqid() calls
    usleep() to avoid collisions, which can be a substantial performance
    hit. See http://blog.kevingomez.fr/til/2015/07/26/why-is-uniqid-slow/
    for details.
    
    Always calling uniqid with $more_entropy = true avoids these problems.

---
+++ Moxio/Sniffs/PHP/DisallowUniqidWithoutMoreEntropySniff.php
@@ -0,0 +1,30 @@
+<?php
+namespace Moxio\Sniffs\PHP;
+
+use Moxio\Sniffs\AbstractFunctionCallSniff;
+use PHP_CodeSniffer\Files\File;
+
+class DisallowUniqidWithoutMoreEntropySniff extends AbstractFunctionCallSniff
+{
+    protected function registerFunctions()
+    {
+        return array('uniqid');
+    }
+
+    protected function processFunctionCall(File $phpcsFile, $functionName, $functionNamePtr, $argumentPtrs)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        if (count($argumentPtrs) < 2) {
+            $error = sprintf('Calls to uniqid() without the $more_entropy-flag are not allowed, because they can be slow', $functionName);
+            $phpcsFile->addError($error, $functionNamePtr, 'ArgumentNotGiven');
+        } else {
+            $moreEntropyArgumentStart = $argumentPtrs[1]['start'];
+            $moreEntropyArgumentEnd = $argumentPtrs[1]['end'];
+            if ($moreEntropyArgumentStart !== $moreEntropyArgumentEnd || $tokens[$moreEntropyArgumentStart]['code'] !== T_TRUE) {
+                $error = sprintf('Calls to uniqid() must have the $more_entropy-flag set to true', $functionName);
+                $phpcsFile->addError($error, $functionNamePtr, 'ArgumentNotTrue');
+            }
+        }
+    }
+}
