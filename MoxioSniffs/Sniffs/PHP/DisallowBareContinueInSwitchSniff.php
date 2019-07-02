a5545c2e0568a960345678bcb87f6ea5cb3e47c8
Arnout Boks <arnout.boks@moxio.com>
2019-07-02 16:53:53 +0200

    Shorten long lines

--- MoxioSniffs/Sniffs/PHP/DisallowBareContinueInSwitchSniff.php
+++ MoxioSniffs/Sniffs/PHP/DisallowBareContinueInSwitchSniff.php
@@ -21,19 +21,27 @@ class DisallowBareContinueInSwitchSniff implements Sniff
         $tokens = $phpcsFile->getTokens();
 
         // We can't process SWITCH statements unless we know where they start and end.
-        if (isset($tokens[$stackPtr]['scope_opener']) === false || isset($tokens[$stackPtr]['scope_closer']) === false) {
+        $hasScopeOpener = isset($tokens[$stackPtr]['scope_opener']);
+        $hasScopeCloser = isset($tokens[$stackPtr]['scope_closer']);
+        if ($hasScopeOpener === false || $hasScopeCloser === false) {
             return;
         }
 
         $switchPtr = $stackPtr;
         $switchClosePtr = $tokens[$switchPtr]['scope_closer'];
 
-        $nextRelevantToken = $switchPtr;
-        while (($nextRelevantToken = $phpcsFile->findNext(array(T_CONTINUE, T_FOR, T_FOREACH, T_WHILE, T_DO), ($nextRelevantToken + 1), $switchClosePtr)) !== false) {
+        $relevantTokens = array(T_CONTINUE, T_FOR, T_FOREACH, T_WHILE, T_DO);
+        $nextRelevantToken = $phpcsFile->findNext($relevantTokens, ($switchPtr + 1), $switchClosePtr);
+        while ($nextRelevantToken !== false) {
             if ($tokens[$nextRelevantToken]['code'] === T_CONTINUE) {
                 $continuePtr = $nextRelevantToken;
 
-                $nextNonEmptyToken = $phpcsFile->findNext(Tokens::$emptyTokens, ($continuePtr + 1), $switchClosePtr, true);
+                $nextNonEmptyToken = $phpcsFile->findNext(
+                    Tokens::$emptyTokens,
+                    ($continuePtr + 1),
+                    $switchClosePtr,
+                    true
+                );
                 if ($tokens[$nextNonEmptyToken]['code'] === T_SEMICOLON) {
                     $error = 'A continue-statement directly inside a switch-case must have a numeric \'level\'-argument';
                     $hint = 'HINT: you probably want `continue 2`, since the switch statement is considered a looping structure';
@@ -43,6 +51,8 @@ class DisallowBareContinueInSwitchSniff implements Sniff
                 // Skip over 'real' looping structures inside the switch statement
                 $nextRelevantToken = $tokens[$nextRelevantToken]['scope_closer'];
             }
+
+            $nextRelevantToken = $phpcsFile->findNext($relevantTokens, ($nextRelevantToken + 1), $switchClosePtr);
         }
     }
 }
